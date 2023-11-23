b496ee40a7d81bb0f2e66d3cff0fe0ca95bf85d6
Jurgita <jurgita.eicinaite@yahoo.com>
2023-11-23 17:04:25 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+canvas.width = 1024;
+canvas.height = 576;
+ctx.fillStyle = 'white';
+ctx.fillRect(10, 10, 50, 50);
